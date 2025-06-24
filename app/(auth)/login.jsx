24f3d68f309005e2f4e0e2b9ab88071222782736
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-24 13:19:27 -0400

    feat: add login button and form submission handler
    - Implemented a login button with a press handler
    - Enhanced user experience by providing feedback on login action

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'expo-router'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
 import ThemedText from '../../components/ThemedText'
 import ThemedView from '../../components/ThemedView'
+import { Colors } from '../../constants/colors'
 
 export default function Login() {
+  const handlePress = () => {
+    console.log('login form submitted')
+  }
+
   return (
     <ThemedView style={styles.container}>
       <Spacer />
@@ -15,6 +21,10 @@ export default function Login() {
       >
         Login to your account
       </ThemedText>
+
+      <ThemedButton onPress={handlePress}>
+        <Text style={{ color: '#f2f2f2' }}>Login</Text>
+      </ThemedButton>
 
       <Spacer height={100} />
 
@@ -32,10 +42,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
     textAlign: 'center',
     fontSize: 18,
     marginBottom: 30,
+  },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8,
   },
 })
