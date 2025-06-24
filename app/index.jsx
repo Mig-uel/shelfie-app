aec51294d6ce358a4260ec091097b9ea87df1656
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-24 10:50:51 -0400

    feat: enhance Home component with logo and text elements
    - Added logo image to the Home component for branding
    - Improved layout with descriptive text for better user experience
    - Updated styles for better visual alignment

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import Logo from '../assets/img/logo_light.png'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    /**
+     * You can think if the native View component as a div and
+     * is mostly used as a wrapper that groups nested components,
+     * and can help with content structure and layout
+     * View component will often be used a root component
+     */
+    <View style={styles.container}>
+      {/* We can either import the image as a module or set the uri */}
+      <Image
+        source={Logo}
+        style={styles.img}
+      />
+
+      {/* The native Text component is used to output text */}
+      <Text
+        // We can also add a mix of inline and external styles
+        style={[
+          styles.title,
+          {
+            color: 'black',
+          },
+        ]}
+      >
+        The Number 1
+      </Text>
+
+      <Text
+        style={{
+          marginTop: 10,
+          marginBottom: 30,
+        }}
+      >
+        Reading list app
+      </Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+/**
+ * Styles can be in its own variable or inline
+ */
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  img: {
+    marginVertical: 20, // Margin for top and bottom
+  },
+})
