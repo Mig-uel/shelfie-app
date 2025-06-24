3782ccaa211f0ee7675856d0f512037ab3d9965a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-24 13:02:13 -0400

    feat: update navigation structure in RootLayout component
    - Adjusted Stack.Screen for authentication flow
    - Set headerShown to false for the authentication screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -25,18 +25,17 @@ export default function RootLayout() {
         }}
       >
         <Stack.Screen
+          name='(auth)'
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
           name='index'
           options={{
             title: 'Home',
           }}
-        />
-        <Stack.Screen
-          name='login'
-          options={{ title: 'Login' }}
-        />
-        <Stack.Screen
-          name='register'
-          options={{ title: 'Register' }}
         />
       </Stack>
     </>
