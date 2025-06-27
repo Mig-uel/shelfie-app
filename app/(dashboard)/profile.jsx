e2d3bfaea0bce398527018b050d5c98897691539
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-27 11:24:37 -0400

    fix: update Profile component layout and logout functionality
    - Adjusted the layout of the Profile component for better
      organization and readability.
    - Added a logout function to enhance user experience.

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -1,22 +1,53 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
 import ThemedText from '../../components/ThemedText'
 import ThemedView from '../../components/ThemedView'
+import { useUserContext } from '../../context/UserContext'
 
 export default function Profile() {
+  const { logout } = useUserContext()
+
+  const handleLogout = () => logout()
+
   return (
-    <ThemedView style={styles.container}>
+    <ThemedView
+      safe
+      style={styles.container}
+    >
+      <Spacer height={10} />
+
       <ThemedText
         title
         style={styles.heading}
       >
-        Your Email
+        My Profile
       </ThemedText>
 
       <Spacer />
 
-      <ThemedText>Time to start reading some books...</ThemedText>
-      <Spacer />
+      <ThemedView
+        style={{
+          justifyContent: 'center',
+          flex: 1,
+        }}
+      >
+        <ThemedText>Time to start reading some books...</ThemedText>
+
+        <Spacer />
+
+        <ThemedButton onPress={logout}>
+          <Text
+            style={{
+              color: '#F2F2F2',
+              textAlign: 'center',
+              fontWeight: 'bold',
+            }}
+          >
+            Logout
+          </Text>
+        </ThemedButton>
+      </ThemedView>
     </ThemedView>
   )
 }
@@ -24,7 +55,7 @@ export default function Profile() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    // justifyContent: 'center',
     alignItems: 'center',
   },
   heading: {
