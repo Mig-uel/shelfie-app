609ba6894b00be26aee0f913b05c207d6bbf7c23
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-28 00:39:33 -0400

    feat: implement FlatList to display user's reading list
    - Added FlatList to render books from context
    - Included ThemedCard for book details
    - Enhanced layout with additional styling

--- app/(dashboard)/books.jsx
+++ app/(dashboard)/books.jsx
@@ -1,9 +1,14 @@
-import { StyleSheet } from 'react-native'
+import { FlatList, Pressable, StyleSheet } from 'react-native'
 import Spacer from '../../components/Spacer'
+import ThemedCard from '../../components/ThemedCard'
 import ThemedText from '../../components/ThemedText'
 import ThemedView from '../../components/ThemedView'
+import { Colors } from '../../constants/colors'
+import { useBooksContext } from '../../context/BooksContext'
 
 export default function Books() {
+  const { books } = useBooksContext()
+
   return (
     <ThemedView
       style={styles.container}
@@ -17,6 +22,24 @@ export default function Books() {
       >
         Your Reading List
       </ThemedText>
+
+      <Spacer />
+
+      <ThemedView style={{ flex: 1 }}>
+        <FlatList
+          data={books}
+          keyExtractor={(book) => book.$id}
+          contentContainerStyle={styles.list}
+          renderItem={({ item }) => (
+            <Pressable>
+              <ThemedCard style={styles.card}>
+                <ThemedText style={styles.title}>{item.title}</ThemedText>
+                <ThemedText style={styles.title}>{item.author}</ThemedText>
+              </ThemedCard>
+            </Pressable>
+          )}
+        />
+      </ThemedView>
     </ThemedView>
   )
 }
@@ -31,6 +54,23 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 18,
     textAlign: 'center',
+  },
+  list: {
+    marginTop: 40,
+  },
+  card: {
+    width: '90%',
+    marginHorizontal: '5%',
+    marginVertical: 10,
+    padding: 10,
+    paddingLeft: 14,
+    borderLeftColor: Colors.primary,
+    borderLeftWidth: 4,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
 })
 
